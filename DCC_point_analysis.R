b3a045522a7c9345c342079b80c4c405d2242387
GuptonLab <34775265+GuptonLab@users.noreply.github.com>
2018-06-07 16:58:46 -0400

    Update DCC_point_analysis.R

--- DCC_point_analysis.R
+++ DCC_point_analysis.R
@@ -1,20 +1,56 @@
-dcc <- read.table(file="C:/Libraries/Documents/DCC_Data_reorganization_final.csv",header=T, sep=",")
+require(ggplot2)
 
-dcc$treat <- factor(dcc$treat)
+dcc <- read.table(file="D:/Caroline/DCC_new_data_analysis_2.csv",header=T, sep=",")
 
-ggplot(dcc, aes(x = treat, y = x)) +
+
+
+dcc$time_taken <- factor(dcc$time_taken)
+
+
+#plot out various interesting stats
+ggplot(dcc, aes(x = time_taken, y = number_of_DCC_clusters)) +
   geom_boxplot(outlier.shape = NA) +
-  geom_line(aes(group = ID)) +
-  geom_point()
+  geom_line(aes(group = ID_pair, color = ID_pair)) +
+  geom_point() + guides(color = FALSE)
 
-dcc_unt <- subset(dcc, treat == 0)
-dcc_treat <- subset(dcc, treat == 1)
-dcc_treat2 <- subset(dcc, treat == 10)
-dcc_treat3 <- subset(dcc, treat == 20)
+#plot out various interesting stats
+ggplot(dcc, aes(x = time_taken, y = cluster_are_perc)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_line(aes(group = ID_pair, color = ID_pair)) +
+  geom_point() + guides(color = FALSE)
+
+
+#plot out various interesting stats
+ggplot(dcc, aes(x = time_taken, y = deltaF)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_line(aes(group = ID_pair, color = ID_pair)) +
+  geom_point() + guides(color = FALSE)
+
+#plot out various interesting stats
+ggplot(dcc, aes(x = time_taken, y = integrated_int)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_line(aes(group = ID_pair, color = ID_pair)) +
+  geom_point() + guides(color = FALSE)
+
+
+
+
+
+
+
+dcc_unt <- subset(dcc, time_taken == 0)
+dcc_treat <- subset(dcc, time_taken == 1)
+dcc_treat2 <- subset(dcc, time_taken == 10)
+dcc_treat3 <- subset(dcc, time_taken == 20)
 
 
 
 #t tests
+t.test(dcc_unt$integrated_int, dcc_treat3$integrated_int, paired = TRUE)
+
+
+
+
 test3 <- t.test(dcc_unt$x, dcc_treat3$x, paired = TRUE)
 test2 <- t.test(dcc_unt$x, dcc_treat2$x, paired = TRUE)
 
